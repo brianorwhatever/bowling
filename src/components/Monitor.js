9755cf51b3a0611586050654949716dd6cc49246
Brian Richter <brianorwhatever@Brians-MacBook-Pro.local>
2016-01-30 16:14:22 -0800

    bottom of monitor started

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -7,21 +7,23 @@ import Scoreboard from '../components/Scoreboard';
 class Monitor extends Component {
   render() {
     return (
-      <div className="monitor-border">
-        <div className="monitor-wrapper">
-          <div className="monitor-shadow">
-            <div className="monitor">
-              <Title />
-              <Scoreboard />
-              <BowlButton />
-              <div className="overlay">AV-1</div>
+      <div className="monitor-container">
+        <div className="monitor-border">
+          <div className="monitor-wrapper">
+            <div className="monitor-shadow">
+              <div className="monitor">
+                {/*<Title />*/}
+                <Scoreboard />
+                <BowlButton />
+                <div className="overlay">AV-1</div>
+              </div>
             </div>
           </div>
         </div>
+        <div className="monitor-bottom"></div>
       </div>
     )
   }
 }
 
 export default Monitor;
-
